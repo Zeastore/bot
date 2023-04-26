90fd4f2ff29416cb135cd854a6ffc9db9d010e69
DTA X ZEAKING PROJECT <98459024+Zeastore@users.noreply.github.com>
2023-04-26 21:28:38 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -44,6 +44,52 @@ DATEVPS=$(date +'%d-%m-%Y')
 TIMEZONE=$(printf '%(%H:%M:%S)T')
 SERONLINE=$(uptime -p | cut -d " " -f 2-10000)
 
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/Zeadxt/kzl/main/ip > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f  /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f  /root/tmp
+}
+# https://raw.githubusercontent.com/Zeadxt/kzl/main/ip 
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/Zeadxt/kzl/main/ip | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/Zeadxt/kzl/main/ip | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+
+
 nginx=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $nginx == "running" ]]; then
     status_nginx="${GREEN}ON${NC}"
@@ -90,6 +136,7 @@ echo -e " ${YELLOW}⇲ DATE & TIME ${NC}         : ${Cyan}$DATEVPS | $TIMEZONE${
 echo -e " ${YELLOW}⇲ DOMAIN ${NC}              : ${Cyan}$domain${NC}"
 echo -e " ${YELLOW}⇲ NS DOMAIN ${NC}           : ${Cyan}$NS${NC}"
 echo -e " ${YELLOW}⇲ IP VPS ${NC}              : ${Cyan}$IPVPS${NC}"
+echo -e " ${YELLOW}⇲ EXP SCRIPT ${NC}          : ${Cyan}$exp${NC}"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "     [ ${Cyan}XRAY:${NC} ${status_xray} ]      [ ${Cyan}NGINX:${NC} ${status_nginx} ]      [ ${Cyan}HAPROXY:${NC} ${status_haproxy} ]"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
